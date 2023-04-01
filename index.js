2c0d8f2eace3f3b6bb9fd67f1c360eb3f9b366d6
Sayedhassan-Hussaini <sh.hussaini1378@gmail.com>
2023-04-01 14:40:28 +0430

    create index file

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+
+// route
+app.get((req,res)=>{
+  res.status(200).json({message:"ok",data:"This is a data...."})
+})
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is ready to port ${port}....`);
+});
