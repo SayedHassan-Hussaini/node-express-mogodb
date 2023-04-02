22ec04d305daf33bd92df39ab80550e5560929f2
Sayedhassan-Hussaini <sh.hussaini1378@gmail.com>
2023-04-02 14:36:08 +0430

    create controller

---
+++ controller/postController.js
@@ -0,0 +1,16 @@
+
+exports.getPost=(req,res)=>{
+  res.status(200).jason({message:"get all post"})
+};
+exports.createPost=(req,res)=>{
+    res.status(200).jason({message:"Create post"})
+  };
+  exports.updatePost=(req,res)=>{
+    res.status(200).jason({message:"Update post"})
+  };
+  exports.deletePost=(req,res)=>{
+    res.status(200).jason({message:"delete post"})
+  };
+  exports.findPost=(req,res)=>{
+    res.status(200).jason({message:"Find post"})
+  };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
+const {
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+  findPost,
+} = require("../controller/postController");
 const router = express.Router();
 
 // Routers
 router.route("/").get(getPost).post(createPost);
 router.route("/:id").get(findPost).patch(updatePost).delete(deletePost);
+
+module.exports = router;
